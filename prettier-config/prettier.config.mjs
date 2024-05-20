f747e3b93655645b47d0e2cf7c625af285fc7a1e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-05-20 14:33:02 +0200

    convert to javascript

---
+++ prettier-config/prettier.config.mjs
@@ -0,0 +1,12 @@
+// http://json.schemastore.org/prettierrc
+// https://prettier.io/docs/en/options.html
+const config = {
+    printWidth: 120,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSameLine: false,
+    arrowParens: 'always',
+    semi: false
+}
+
+export default config
